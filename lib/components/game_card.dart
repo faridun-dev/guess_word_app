338ce6008c4abd086f200cf69456b65855ea2109
faridun-dev <faridunf200630@gmail.com>
2024-08-08 16:32:10 +0500

    Create Home Screen and its UI

---
+++ lib/components/game_card.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class GameCard extends StatelessWidget {
+  String title;
+  GameCard({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 20,
+        left: 20,
+        right: 20,
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(
+            12,
+          ),
+        ),
+        padding: const EdgeInsets.all(
+          40,
+        ),
+        width: double.infinity,
+        child: Center(
+          child: Text(
+            title,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 23,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:guess_word_app/components/game_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,12 +12,24 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[500],
+      backgroundColor: Colors.blue[600],
       appBar: AppBar(
         title: const Text(
-          "Words game",
+          "Words Game",
+          style: TextStyle(
+            fontSize: 26,
+          ),
         ),
+        centerTitle: true,
+        foregroundColor: Colors.white,
         backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        children: [
+          GameCard(title: "Learn Words 👨🏻‍🎓"),
+          GameCard(title: "Guess Words 🤔"),
+          GameCard(title: "Match Words 🎯"),
+        ],
       ),
     );
   }
